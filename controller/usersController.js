b43ba3d8582da481844914af48cbf123d1811930
MarcosAugusto10 <marcosaugusto@sbsoftware.com.br>
2025-07-22 01:26:16 -0300

    Add Controller, models, routes

---
+++ controller/usersController.js
@@ -0,0 +1,28 @@
+const User = require('../models/user');
+
+exports.getAllUsers = async(req, res)  =>
+{
+    try
+    {
+        const users= await User.find();
+    }
+    catch(err)
+    {
+        res.status(500).send(err);
+    }
+};
+
+exports.CreateUser = async(req, res)=>
+{
+    const { name,email,age,lastname} = req.body;
+    try
+    {
+        const user = new User({ name,email,age,lastname});
+        await user.save();
+    }
+    catch(err)
+    {
+        res.status(500).send(err);
+    }
+
+}
